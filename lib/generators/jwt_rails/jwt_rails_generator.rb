cfd0579c5b86f711702f5666be3e1dbeffc1b1cd
Chang-Woo Rhee <changwoo.rhee@kooli.tv>
2019-12-20 16:04:45 -0800

    Added copy file

--- lib/generators/jwt_rails/jwt_rails_generator.rb
+++ lib/generators/jwt_rails/jwt_rails_generator.rb
@@ -1,3 +1,25 @@
-class JwtRailsGenerator < Rails::Generators::NamedBase
-  source_root File.expand_path('templates', __dir__)
+class JwtRailsGenerator < Rails::Generators::Base
+#  source_root File.expand_path('templates', __dir__)
+  
+  def copy_file
+    # Use Json Web Token (JWT) for token based authentication
+    gem 'jwt'
+    # Use ActiveModel has_secure_password
+    gem 'bcrypt', '~> 3.1.7'
+
+    route "resources :users, param: :_username"
+    route "post '/auth/login', to: 'authentication#login'"
+    route "get '/*a', to: 'application#not_found'"
+
+    copy_file "json_web_token.rb", "app/lib/json_web_token.rb"
+    copy_file "application_controller.rb", "app/controllers/application_controller.rb"
+
+    generate "model", "user name:string username:string email:string password_digest:string"
+    copy_file "user.rb", "app/models/user.rb"
+    generate "controller", "users"
+    copy_file "users_controller.rb", "app/controllers/users_controller.rb"
+    generate "controller", "authentication"
+    copy_file "authentication_controller.rb", "app/controllers/authentication_controller.rb"
+  end
+
 end
